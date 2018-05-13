2e737d59e5cc0f79d678ad62009362624ba7fab0
MichaelKW666 <39233337+MichaelKW666@users.noreply.github.com>
2018-05-13 15:18:29 +0800

    Update tor1.py

--- Cha 11 -服务器采集/tor1.py
+++ Cha 11 -服务器采集/tor1.py
@@ -3,7 +3,7 @@ import socks
 import requests
 
 # Tor使用9150端口为默认的socks端口
-socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9150)
+socks.set_default_proxy(socks.SOCKS5, "localhost", 9150)
 socket.socket = socks.socksocket
 # 获取这次抓取使用的IP地址
 a = requests.get("http://checkip.amazonaws.com").text
